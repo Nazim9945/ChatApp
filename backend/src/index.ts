1e0fd315253d4f032ba6b8b249d9226ed8a97e97
Nazim Saifi <saifinazim2311@gmail.com>
2026-05-10 12:38:39 +0530

    running now

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,17 +3,28 @@ import {WebSocket,WebSocketServer} from 'ws'
 const wss=new WebSocketServer({port:8080})
 let count=0;
 
-wss.on("message",(socket:WebSocket)=>{
+wss.on("connection",(socket:WebSocket)=>{
     count++;
     console.log("user connect ",count)
 
 
-    socket.on('message',(messgae)=>{
-        socket.send(messgae.toString())
+    socket.on('message',(message)=>{
+        
+        if(message.toString()==="bye"){
+            socket.send("disconnecting u now....")
+            socket.close()
+        }
+        else{
+            socket.send("pong pong")
+        }
+       
+    })
+    socket.send("pong")
 
+    socket.on('close',()=>{
+        console.log("disconnecting")
     })
-    socket.on('disconnet',()=>{
-        console.log("user disconnect")
-        socket.send("disconnected");
-    })
+  
+   
 })
+
