df2a274b526d418c28fc83677d7985e6a750550e
Nazim Saifi <saifinazim2311@gmail.com>
2026-05-10 15:10:10 +0530

    added room logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,37 @@
-import {WebSocket,WebSocketServer} from 'ws'
+import { WebSocket, WebSocketServer } from "ws";
 
-const wss=new WebSocketServer({port:8080})
-let count=0;
+const wss = new WebSocketServer({ port: 8080 });
+let count = 0;
 
-wss.on("connection",(socket:WebSocket)=>{
-    count++;
-    console.log("user connect ",count)
+let allSockets = new Map<string, WebSocket[]>();
+wss.on("connection", (socket: WebSocket) => {
+  count++;
+  console.log("user connect ", count);
 
+  socket.on("message", (message) => {
+    const parsed = JSON.parse(message as unknown as string);
+    console.log(parsed)
+    if (parsed.type === "join room") {
+      const roomId = parsed.payload.roomId;
 
-    socket.on('message',(message)=>{
-        
-        if(message.toString()==="bye"){
-            socket.send("disconnecting u now....")
-            socket.close()
-        }
-        else{
-            socket.send("pong pong")
-        }
-       
-    })
-    socket.send("pong")
+      if (!allSockets.has(roomId)) {
+        allSockets.set(roomId, [socket]);
+      } else {
+        allSockets.get(roomId)?.push(socket);
+      }
+      console.log("joined the room!!")
+    } else if (parsed.type === "chat") {
+      const roomId = parsed.payload.roomId;
+      const arr: WebSocket[] = allSockets.get(roomId) || [];
 
-    socket.on('close',()=>{
-        console.log("disconnecting")
-    })
-  
-   
-})
-
+      arr.map((socket) => {
+        socket.send(JSON.stringify(parsed));
+      });
+    }
+    
+  });
+  socket.on("close", () => {
+    console.log("disconnecting");
+  });
+  socket.send("pong");
+});
