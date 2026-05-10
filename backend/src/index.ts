46055dd570d7a6d4261c1ca6d49ffe1019b4e512
Nazim Saifi <saifinazim2311@gmail.com>
2026-05-10 19:24:50 +0530

    setup frontend and backend togather

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,11 @@ import { WebSocket, WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 let count = 0;
-
-let allSockets = new Map<string, WebSocket[]>();
+interface User{
+    socket:WebSocket,
+    username:string
+}
+let allSockets = new Map<string, User[]>();
 wss.on("connection", (socket: WebSocket) => {
   count++;
   console.log("user connect ", count);
@@ -11,27 +14,41 @@ wss.on("connection", (socket: WebSocket) => {
   socket.on("message", (message) => {
     const parsed = JSON.parse(message as unknown as string);
     console.log(parsed)
-    if (parsed.type === "join room") {
+    if (parsed.type === "join") {
       const roomId = parsed.payload.roomId;
+      const username=parsed.payload.username
 
       if (!allSockets.has(roomId)) {
-        allSockets.set(roomId, [socket]);
+        allSockets.set(roomId, [{socket, username}]);
       } else {
-        allSockets.get(roomId)?.push(socket);
+        allSockets.get(roomId)?.push({socket,username});
       }
       console.log("joined the room!!")
-    } else if (parsed.type === "chat") {
+    } 
+    if (parsed.type === "chat") {
       const roomId = parsed.payload.roomId;
-      const arr: WebSocket[] = allSockets.get(roomId) || [];
+      const arr: User[] = allSockets.get(roomId) || [];
 
-      arr.map((socket) => {
-        socket.send(JSON.stringify(parsed));
+      arr.map((data) => {
+        data.socket.send(JSON.stringify(parsed));
       });
+    }
+
+    if(parsed.type==="leave"){
+         const roomId = parsed.payload.roomId;
+         let arr:User[]=allSockets.get(roomId) || [];
+         if(arr.length==0) {
+            allSockets.delete(roomId)
+         }
+         else{
+            arr=arr.filter((soc)=>soc.socket!=socket)
+            allSockets.set(roomId,arr);
+         }
     }
     
   });
   socket.on("close", () => {
     console.log("disconnecting");
   });
-  socket.send("pong");
+  
 });
---
+++ frontend/src/App.tsx
@@ -0,0 +1,73 @@
+import { motion } from "motion/react"
+import { useEffect, useRef } from "react";
+
+
+function App(){
+const nameRef=useRef<HTMLInputElement | null>(null)
+const roomRef = useRef<HTMLInputElement | null>(null);
+const wsRef = useRef<WebSocket | null>(null);
+useEffect(()=>{
+const ws=new WebSocket('http://localhost:8080')
+wsRef.current=ws
+ws.onmessage=(event)=>{
+  const parsed=JSON.parse(event.data);
+  console.log(parsed)
+  // token
+
+
+}
+},[])
+
+const handler = (e: React.SubmitEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  if(nameRef.current && roomRef.current){
+    console.log(nameRef.current.value)
+    console.log(roomRef.current.value);
+    let obj={
+      type:"join",
+      payload:{
+        username:nameRef.current.value,
+        roomId:roomRef.current.value
+      }
+    } 
+    const  message=JSON.stringify(obj)
+    if(wsRef.current){
+      wsRef.current.onopen=()=>{
+        wsRef.current?.send(message)
+        alert("U have sent the message for creating room!!")
+      }
+    }
+    
+  }
+};
+    return (
+      <div className="h-screen bg-black flex items-center justify-center">
+        <motion.form
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 0.5, duration: 1 }}
+          className="w-120 flex flex-col  border border-white shadow shadow-pink-300   gap-4 h-80 bg-gray-500 p-4 items-center justify-center rounded-lg"
+          onSubmit={handler}
+        >
+          <input
+            ref={nameRef}
+            className="w-full py-3 px-1 bg-gray-700 text-white/80 outline-none focus:border focus:border-gray-600 flex rounded-lg"
+            placeholder="Username"
+           
+          />
+          <input
+            ref={roomRef}
+            className="w-full py-3 px-1 bg-gray-700 text-white/80 outline-none focus:border focus:border-gray-600 flex rounded-lg"
+            placeholder="Enter RoomName..."
+          />
+          <button className="w-full py-3 px-1 rounded-lg cursor-pointer capitalize bg-red-600/70 active:scale-95 transition-all">
+            create room
+          </button>
+        </motion.form>
+      </div>
+    );
+
+
+
+}
+export default App
