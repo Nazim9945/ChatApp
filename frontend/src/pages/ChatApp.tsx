e35f0ed7a53809c06d6bb0b402dea24470838d45
Nazim Saifi <saifinazim2311@gmail.com>
2026-05-15 17:56:19 +0530

    add enter key

--- frontend/src/pages/ChatApp.tsx
+++ frontend/src/pages/ChatApp.tsx
@@ -16,24 +16,25 @@ export const ChatApp=({socket,message,setMessage}:Props)=>{
 const inputRef=useRef<HTMLInputElement|null>(null)
 const handler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
   e.preventDefault();
-  if (inputRef.current) {
-     let obj = {
-       type: "chat",
-       payload: {
-         username:name ,
-         roomId:room,
-         message:inputRef.current.value
-       },
-     };
-     const message = JSON.stringify(obj);
-    socket?.send(message);
-    // setMessage(prev=>[...prev,inputRef.current?.value || "")--->Type of this?
-    // @ts-ignore
-    // setMessage(inputRef.current.value)
-    setMessage(obj.payload)
-   
-    inputRef.current.value = "";
-  }
+  if (!inputRef.current?.value) return;
+    if (inputRef.current) {
+      let obj = {
+        type: "chat",
+        payload: {
+          username: name,
+          roomId: room,
+          message: inputRef.current.value,
+        },
+      };
+      const message = JSON.stringify(obj);
+      socket?.send(message);
+      // setMessage(prev=>[...prev,inputRef.current?.value || "")--->Type of this?
+      // @ts-ignore
+      // setMessage(inputRef.current.value)
+      setMessage(obj.payload);
+
+      inputRef.current.value = "";
+    }
 };
 useEffect(()=>{
  bottomRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -41,8 +42,10 @@ useEffect(()=>{
     return (
       <div className="h-screen bg-black flex items-center justify-center">
         <div className="absolute right-5 top-10 h-14 w-14 rounded-full border-2 border-white/90 p-8 flex items-center justify-center bg-gray-500">
-         <span className="font-bold text-2xl  text-yellow-100">{name?.substring(0,2).toUpperCase() || "UK"}</span>
-        </div> 
+          <span className="font-bold text-2xl  text-yellow-100">
+            {name?.substring(0, 2).toUpperCase() || "UK"}
+          </span>
+        </div>
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -50,11 +53,11 @@ useEffect(()=>{
           className="w-180 mx-auto flex flex-col  border border-white shadow shadow-pink-300   gap-4 h-100 bg-gray-500 p-4  justify-center rounded-lg"
         >
           <div className="h-[80%] flex flex-col  gap-2 overflow-x-scroll  no-scrollbar py-2 relative">
-            {message.map((mes,index) => {
+            {message.map((mes, index) => {
               return (
                 <div
                   key={index}
-                  className={`relative max-w-[40%]  wrap-break-word rounded-md shadow drop-shadow-black py-2 px-1 ${mes.username===name ? 'self-end bg-blue-700 text-white' : 'self-start bg-yellow-300 '}`}
+                  className={`relative max-w-[40%]  wrap-break-word rounded-md shadow drop-shadow-black py-2 px-1 ${mes.username === name ? "self-end bg-blue-700 text-white" : "self-start bg-yellow-300 "}`}
                 >
                   {mes.message}
                 </div>
@@ -68,8 +71,35 @@ useEffect(()=>{
               type="text"
               placeholder="Enter messages..."
               ref={inputRef}
+              required
+              onKeyDown={(e) => {
+                
+                if(e.key==='Enter'){
+                  e.preventDefault()
+                  if (!inputRef.current?.value) return;
+                  if (inputRef.current) {
+                    let obj = {
+                      type: "chat",
+                      payload: {
+                        username: name,
+                        roomId: room,
+                        message: inputRef.current.value,
+                      },
+                    };
+                    const message = JSON.stringify(obj);
+                    socket?.send(message);
+                  //  @ts-ignore
+                    setMessage(obj.payload);
+
+                    inputRef.current.value = "";
+                  }
+                }
+              }}
             />
-            <button className="border p-1" onClick={handler}>
+            <button
+              className="border border-blue-700  p-3 text-xl rounded-md bg-blue-700 text-white"
+              onClick={handler}
+            >
               <BsSend />
             </button>
           </div>
